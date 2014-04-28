6ce5f5c8bd3eec2cd841c95f515ea94378e5a463
Jordan S Read <jread@usgs.gov>
2014-04-28 15:14:24 -0500

    function for epsilon fitting from chunked data

---
+++ R/fit.epsilon.R
@@ -0,0 +1,39 @@
+#'@title fit epsilon from block of ADV data
+#'@description 
+#'solves for epsilon from ADV data in the inertial subrange assuming Zappa et al. 2003  \cr
+#'
+#'@details a \code{velocimeterK} function for splitting sensor data into temporal segments.\cr 
+#'
+#'@references
+#'Zappa, Christopher J., Peter A. Raymond, Eugene A. Terray, and Wade R. McGillis. 
+#'"Variation in surface turbulence and the gas transfer velocity over a tidal cycle in a macro-tidal estuary." 
+#'Estuaries 26, no. 6 (2003): 1401-1415.
+#'
+#'@param \code{chunk.adv} a data.frame created with load.ADV, with the window.idx column
+#'@param \code{freq} a numeric, in Hz, that represents the input data
+#'@param \code{lower} a numeric, in Hz, that represents the lower bounds for range of epsilon calculation
+#'@param \code{upper} a numeric, in Hz, that represents the upper bounds for range of epsilon calculation
+#'@return a single value of epsilon estimating from the temporal block given. 
+#'@keywords fit.epsilon
+#'@import oce
+#'@examples 
+#'folder.nm <- '/Users/jread/Documents/R/velocimeterK/supporting data/'
+#'file.nm <- "ICACOS04.dat"
+#'data.adv <- load.ADV(file.nm=file.nm, folder.nm =folder.nm)
+#'window.adv <- window.ADV(data.adv,freq=32,window.mins=10)
+#'fit.epsilon(window.adv[window.adv$window.idx==1, ],freq=32)
+#'@export
+#'
+
+fit.epsilon <- function(chunk.adv,freq=32, lower= 0.5,upper=8){
+  
+  xts <- ts(chunk.adv$velocity.Z, frequency=freq)
+  w <- pwelch(xts, plot=FALSE)
+  wavenum.spectra <- 2*pi*w$spec/mean(abs(chunk.adv$velocity.Y))
+  wavenum <- w$freq
+  
+  use.i <- lower <= wavenum & wavenum <= upper
+  mod.num <- 0.52*(wavenum^(-5/3))
+  epsilon <- lm(wavenum.spectra[use.i]~mod.num[use.i] -1)$coeff[[1]]
+  return(epsilon)
+}
