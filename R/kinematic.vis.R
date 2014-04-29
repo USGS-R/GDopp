5071dc89d0c1e2c8fd57cafaab9e2ac3639116f9
Jordan S Read <jread@usgs.gov>
2014-04-29 07:22:39 -0500

    methods for kinematic vis of water

---
+++ R/kinematic.vis.R
@@ -0,0 +1,19 @@
+#'@title get kinematic viscosity
+#'@param \code{temperature} Temperature in °C of water
+#'@return kinematic viscosity
+#'@keywords kinematic.vis
+#'@examples 
+#'kin.vis <- kinematic.vis(temperature=20)
+#'@export
+
+kinematic.vis <- function(temperature=20) {
+  # from Mays 2005, Water Resources Engineering
+  tempTable <- seq(0,100,by=5)
+  # table in m2/s E-6
+  visTable <- c(1.792,1.519,1.308,1.141,1.007,0.897,
+                0.804,0.727,0.661,0.605,0.556,0.513,0.477,0.444,
+                0.415,0.39,0.367,0.347,0.328,0.311,0.296)
+  v <- data.frame(approx(tempTable,visTable,xout = temperature))[2]
+  v <- v*1e-6
+  return(v$y)
+}
