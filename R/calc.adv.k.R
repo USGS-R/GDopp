3b8042e96d4bcd5562898e1ab1f1e82fb4bb1f6e
Jordan S Read <jread@usgs.gov>
2014-05-12 20:33:05 -0500

    log for fitting technique

--- R/calc.adv.k.R
+++ R/calc.adv.k.R
@@ -6,19 +6,20 @@ calc.adv.k <- function(deploy.name='ALQ102'){
   file.nm <- paste0(deploy.name,'.dat')
   data.adv <- load.ADV(file.nm=file.nm, folder.nm =folder.nm)
   window.adv <- window.ADV(data.adv,freq=freq,window.mins=10)
-  num.wins <- length(unique(window.adv$window.idx))
+  
   
   data.sen <- load.sen(file.nm=paste0(deploy.name,'.sen'))
   temp.df <- temp.calc(data.sen,window.adv$window.idx,freq=freq,calc.time=TRUE)
   temp.block = temp.df$temperature
   temp.time <- temp.df$time
-  
+  num.wins <- length(temp.time)
   k.out <- vector(length=num.wins)
   
   for (i in 1:num.wins){
+    cat(i); cat(' of '); cat(num.wins); cat('\n')
     chunk.adv <- window.adv[window.adv$window.idx==i, ]
     if (!check.adv(chunk.adv=chunk.adv)){
-      epsilon <- fit.epsilon(chunk.adv,freq=freq)
+      epsilon <- fit.epsilon(chunk.adv,freq=freq,lower= 20,upper=80,diagnostic=TRUE)
       k.out[i] <- epsilon2k(epsilon,temperature=temp.block[i],nu=nu) 
     } else {
       k.out[i] <- NA
--- R/fit.epsilon.R
+++ R/fit.epsilon.R
@@ -13,6 +13,7 @@
 #'@param \code{freq} a numeric, in Hz, that represents the input data
 #'@param \code{lower} a numeric, in Hz, that represents the lower bounds for range of epsilon calculation
 #'@param \code{upper} a numeric, in Hz, that represents the upper bounds for range of epsilon calculation
+#'@param \code{diagnostic} a boolean for diagnostic plot
 #'@return a single value of epsilon estimating from the temporal block given. 
 #'@keywords fit.epsilon
 #'@import oce
@@ -25,7 +26,7 @@
 #'@export
 #'
 
-fit.epsilon <- function(chunk.adv,freq=32, lower= 20,upper=80){
+fit.epsilon <- function(chunk.adv,freq=32, lower= 20,upper=80,diagnostic = FALSE){
   
   xts <- ts(chunk.adv$velocity.Z, frequency=freq)
   w <- pwelch(xts, plot=FALSE)
@@ -34,17 +35,31 @@ fit.epsilon <- function(chunk.adv,freq=32, lower= 20,upper=80){
   v.mn <- v.calc(chunk.adv)
   
   wavenum <- 2*pi*w$freq/v.mn # in radians/m
+  lower.k = lower/v.mn
+  upper.k = upper/v.mn
   
-  use.i <- lower <= wavenum & wavenum <= upper
+  use.i <- lower.k <= wavenum & wavenum <= upper.k
   
   f <- function (x, w.s, k){
     
-    eps.2.3 <- x^(2/3)
-    epsilon.rmse <- rmse(log((0.52*eps.2.3*k)*k^(-5/3)),log(w.s))
+    #eps.2.3 <- x^(2/3)
+    e.log = x
+    epsilon.rmse <- rmse(log(0.52*k^(-5/3))+e.log,log(w.s))
     
     return(epsilon.rmse)
   }
-  epsilon <- optimize(f, c(10^(-10), 1), tol = 0.0001, w.s = wavenum.spectra[use.i], k = wavenum[use.i])$minimum
+  
+  epsilon.2.3.log <- optimize(f, c(-100,1000), tol = 0.0001, w.s = wavenum.spectra[use.i], k = wavenum[use.i])$minimum
+  
+  epsilon = (exp(epsilon.2.3.log))^3/2
+  if (diagnostic){
+    
+    plot(wavenum,wavenum.spectra,log='xy')
+    abline(v=lower.k)
+    abline(v=upper.k)
+    eps.2.3 = epsilon^(2/3)
+    lines(wavenum,0.52*eps.2.3*wavenum^(-5/3),col='green')
+  }
   
   return(epsilon)
 }
