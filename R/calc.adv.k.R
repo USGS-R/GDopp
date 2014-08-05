f4c940c1f95dbcffdc20dbe9ac67f17f5e376225
Jordan S Read <jread@usgs.gov>
2014-08-05 15:07:48 -0500

    adding transform coords & folder.nm

--- R/calc.adv.k.R
+++ R/calc.adv.k.R
@@ -9,16 +9,22 @@ calc.adv.k <- function(deploy.name='ALQ102'){
   window.adv <- window_ADV(data.adv,freq=freq,window.mins=3)
   
   
-  data.sen <- load.sen(file.nm=paste0(deploy.name,'.sen'))
+  data.sen <- load.sen(file.nm=paste0(deploy.name,'.sen'), folder.nm)
+  coord.df <- window_coord(data.sen, window.adv$window.idx, freq = freq)
   temp.df <- temp.calc(data.sen,window.adv$window.idx,freq=freq,calc.time=TRUE)
   temp.block <- temp.df$temperature
   temp.time <- temp.df$time
   num.wins <- length(temp.time)
   k.out <- vector(length=num.wins)
+  trans_data <- c(2896, 2896, 0, -2896, 2896, 0, -2896, -2896, 5792) / 4096
+  trans_matrix <- matrix(data = trans_data, ncol = 3, byrow = TRUE) # get this from the file!!!!
   
   for (i in 1:num.wins){
     cat(i); cat(' of '); cat(num.wins); cat('\n')
     chunk.adv <- window.adv[window.adv$window.idx==i, ]
+    
+    ENU_adv <- coord_transform(trans_matrix, data_v = chunk.adv, position_data=coord.df[i, ])
+    
     tests <- c('frozen.turb.check_adv','beam.correlation.check_adv')
     #tests <- 'all'
     cck <- check.adv(chunk.adv=chunk.adv, tests, verbose=TRUE)
