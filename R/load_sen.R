7aa9014f57e64d730fa81bc59c3a22f9976d7394
Jordan S Read <jread@usgs.gov>
2014-08-21 08:50:58 -0500

    putting helpers in load_sen

--- R/load_sen.R
+++ R/load_sen.R
@@ -43,3 +43,56 @@ load_sen <- function(file.nm, folder.nm){
   
   return(data.sen)
 }
+
+
+match_time <- function(value, window.idx, freq=32){
+  if (!is.null(dim(window.idx))){stop('window.idx must be a 1D vector')}
+  
+  num_values <- ifelse(is.null(dim(value)),1,ncol(value))
+  
+  num.diag <- nrow(data.sen)
+  num.hf <- length(window.idx)
+  
+  expect.hf <- num.diag*freq
+  hf.dif <- expect.hf-num.hf 
+  
+  step.drop <- ceiling(hf.dif/freq)
+  if (hf.dif<0){stop('actual high-frequency measurements should not exceed frequency*diagnostic file. Check files')}
+  
+  if (step.drop>=1){
+    data.sen <- data.sen[-1, ]
+  }
+  
+  
+  t.win <- window.idx[seq(1,length(window.idx),freq)]
+  
+  #if (length(value) != length(t.win)){stop('win blocks are different lengths than measurement array')}
+  
+  pad.num <- nrow(data.sen) - length(t.win)
+  rep.pad <- rep(x=tail(t.win,1),pad.num)
+  t.win <- c(t.win,rep.pad)
+  un.blocks <- unique(t.win)
+  
+  length.out <- length(un.blocks)
+  
+  block.value <- vector(length = length.out)#matrix(nrow = length.out, ncol = num_values) FUTURE!!!
+  time <- rep(as.POSIXct('1900-01-01'),length.out)
+  
+  for (i in seq_len(length(un.blocks))){
+    block.value[i] <- mean(value[t.win==un.blocks[i]])
+    time[i] <- get.sen.time(chunk.sen=data.sen[t.win==un.blocks[i], ])
+  }
+  
+  return(data.frame('time'=time, 'value'=block.value))
+}
+
+get.sen.time <- function(chunk.sen){
+  d.vals <- chunk.sen[, 1:6]
+  d.tail <- as.numeric(tail(d.vals,1))
+  d.head <- as.numeric(head(d.vals,1))
+  date.1 <- ISOdatetime(d.head[3],d.head[1],d.head[2],d.head[4],d.head[5],d.head[6])
+  date.2 <- ISOdatetime(d.tail[3],d.tail[1],d.tail[2],d.tail[4],d.tail[5],d.tail[6])
+  
+  mn.date <- mean(c(date.1,date.2))
+  return(mn.date)
+}
